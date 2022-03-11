e0d1f0f4e1e611b2c1b9fe9854433481b3a34bf2
Scheibelr <scheibelr@gmail.com>
2022-03-11 09:54:43 -0500

    Router syntax updated to reactrouterdomv6

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { BREED } from '../shared/breed';
-import BreedInfo from './BreedInfoComponent';
+
 
 
 class Main extends Component {
@@ -16,6 +16,7 @@ class Main extends Component {
       //selectedBreed: null
     };
   }
+
 
  /* onBreedSelect(breedId) {
     this.setState({selectedBreed: breedId});
@@ -33,10 +34,10 @@ class Main extends Component {
       <div>
         <Header />
         <Routes>
-          {/* Why is it that on the next line, the word "element" is so special and has to specifically be used. */}
+          {/* Why is it that on the next line, the word "element" is so special and has to specifically be used. Answer: Because that's what it says in the docs!*/}
+          <Route path='/home' element={<Home/>} />
+          <Route exact path='/directory' element={<Directory breed={this.state.breed}/>} />
           <Route path='/' element={<Navigate replace to="/home" />} />
-          <Route path='/home' component={HomePage} />
-          <Route path='/directory' render={() => <Directory breed={this.state.breed}/>} />
         </Routes>
         <Footer />
       </div>
