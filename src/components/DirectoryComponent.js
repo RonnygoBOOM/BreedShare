e4d30f2fdf4901e1ae90906e70bea4d21c0ea015
Scheibelr <scheibelr@gmail.com>
2022-03-11 21:52:46 -0500

    WIP

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,10 +3,10 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
-function RenderDirectoryItem({breed}) {
+function RenderDirectoryItem({breed, onClick}) {
     return (
             <div key={breed.id}>
-                <Card className="directory-card mt-4 m-1">
+                <Card className="directory-card mt-4 m-1" onClick={() => onClick(breed.id)}>
                     <Link to={`/directory/${breed.id}`}></Link>
                     <CardImg width="100%" className="directory-image" src={breed.image} alt={breed.name} />
                     <CardImgOverlay>
@@ -23,7 +23,7 @@ function Directory(props) {
     const directory = props.breed.map(breed => {
         return (
             <div className="col">
-                <RenderDirectoryItem breed={breed} />
+                <RenderDirectoryItem breed={breed} onClick={props.onClick}/>
             </div>
         );
     });
@@ -37,5 +37,3 @@ function Directory(props) {
 }
 
 export default Directory;
-
-
